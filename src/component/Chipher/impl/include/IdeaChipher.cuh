d0143025438b9650808e4e2e43d72335212f59f8
dago <d_goo@outlook.jp>
2026-07-30 11:02:38 +0000

    変更

--- src/component/Chipher/impl/include/IdeaChipher.cuh
+++ src/component/Chipher/impl/include/IdeaChipher.cuh
@@ -1,15 +1,17 @@
 #include "../../interface/IChipher.cuh"
+class IIdeaMA;
 
 class IdeaChipher : public IChipher
 {
 private:
-    u_short *_plaintxt;
-    u_short _chiphertxt[4];
     u_short *_eKey;
     u_short *_dKey;
+    const IIdeaMA &_mulInv;
+    const IIdeaMA &_mul;
+    const IIdeaMA &_add;
 
 public:
-    IdeaChipher(const u_short (&plaintxt)[4], const u_short (&ekey)[52], const u_int (&dkey)[52]);
-    void encrypt();
-    void decrypt();
+    IdeaChipher(const IIdeaMA &mulInv, const IIdeaMA &mul, const IIdeaMA &add, const u_short (&ekey)[52], const u_int (&dkey)[52]);
+    void encrypt(const u_short (&plaintxt)[4], u_short *cipherTxt);
+    void decrypt(const u_short (&cipherTxt)[4]);
 };
--- src/component/Chipher/impl/src/IdeaChipher.cu
+++ src/component/Chipher/impl/src/IdeaChipher.cu
@@ -1,3 +1,25 @@
 #include "../include/IdeaChipher.cuh"
+#include "../../../MA/interface/IIdeaMA.cuh"
 
-IdeaChipher::IdeaChipher(const u_short (&plaintxt)[4], const u_short (&ekey)[52], const u_int (&dkey)[52]) :
+IdeaChipher::IdeaChipher(const IIdeaMA &mulInv, const IIdeaMA &mul, const IIdeaMA &add, const u_short (&ekey)[52], const u_int (&dkey)[52]) : _mulInv(mulInv), _mul(mul), _add(add)
+{
+
+    _eKey = new u_short[52];
+    for (int i = 0; i < 52; i++)
+    {
+        _eKey[i] = ekey[i]; // 52個の暗号化鍵を格納する配列(0~47までは8段目までの暗号化鍵、48~51までは最終段の暗号化鍵)
+    }
+
+    _dKey = new u_short[52];
+    for (int i = 0; i < 52; i++)
+    {
+        _dKey[i] = dkey[i]; // 52個の復号化鍵を格納する配列(0~47までは8段目までの復号化鍵、48~51までは最終段の復号化鍵)
+    }
+}
+
+void IdeaChipher::encrypt(const u_short (&plaintxt)[4], u_short *cipherTxt)
+{
+}
+void IdeaChipher::decrypt(const u_short (&cipherTxt)[4])
+{
+}
