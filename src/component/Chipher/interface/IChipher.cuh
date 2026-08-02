89841183d060a46a88bd12e940e9adf924dbaf58
dago <d_goo@outlook.jp>
2026-08-02 14:39:33 +0000

    変更した

---
+++ src/component/Chipher/interface/IChipher.cuh
@@ -0,0 +1,6 @@
+class IChipher
+{
+public:
+    virtual ~IChipher() = default;
+    virtual void Chipher(const IIdeaMA &_mulInv, const IIdeaMA &_mul, const IIdeaMA &_add, const u_short (&_dKey)[52]) = 0;
+};
--- src/main.cu
+++ src/main.cu
@@ -1,8 +1,13 @@
 #include <stdio.h>
-#include "./component/MA/impl/include/Multiplication.cuh"
-#include 
+#include <iostream>
+#include "./component/factory.cuh"
 
 int main(void)
 {
+    char key[18], plainTxt[100];
+    std::cout << "-----Please input key:-----" << std::endl;
+    
+    std::cin.getline(key, 18);
+
     return 0;
 }
